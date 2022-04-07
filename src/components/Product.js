cc3bbd2ffa04e23735ea54659c54b8cd408d5fc6
zand-yasin <zand.kurdy@gmail.com>
2022-04-07 05:35:49 +0300

    Product + HomeScreen + Rating

---
+++ src/components/Product.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+import PropTypes from 'prop-types'
+
+const Product = ({ product }) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong> {product.name} </strong>{' '}
+          </Card.Title>
+        </a>
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            // color='red'
+          />
+        </Card.Text>
+        <Card.Text as='h3'>$ {product.price} </Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Product
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import products from '../products'
-import {Row, Col} from 'react-bootstrap'
+import Product from '../components/Product'
+import { Row, Col } from 'react-bootstrap'
 const HomeScreen = () => {
   return (
     <>
-    <h1>Latest Products</h1>
-    <Row>
-        {products.map(product=> (
-            <Col sm={12} md={6} lg={4} xl={3}  >
-            <h3>{product.name}</h3>
-            </Col>
-        )  )}
-        </Row>    
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
     </>
   )
 }
