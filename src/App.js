9419b554f79903b2495955f1ae77b40761ee63b9
zand-yasin <zand.kurdy@gmail.com>
2022-04-07 07:40:02 +0300

    add route + product detail

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
-import {Container} from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 const App = () => {
-
   return (
-    <> 
+    <Router>
       <Header />
-    <main className='py-3' >
-      <Container>
-      <HomeScreen />
-
-      </Container>
-    </main>
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            {/* <Route path='/' component={HomeScreen} exact /> */}
+          </Routes>
+        </Container>
+      </main>
       <Footer />
-    </>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
